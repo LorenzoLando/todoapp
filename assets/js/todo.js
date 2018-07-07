6e886970173c3df883a326cb19c2284b40a76ab5
Lorenzo <lorenzo.lando85@gmail.com>
2018-07-07 15:40:50 +0100

    finished basic functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,33 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click","li", function(){
 	$(this).toggleClass("completed");
 });
+
+$("ul").on("click","span", function(event){
+	//faccio fade out dell`elemento parent dello span
+	$(this).parent().fadeOut(500, function() {
+		//utilizzo una funzione di callbak
+		//in questo modo remove() avviene DOPO fadeout
+		$(this).remove();
+	});
+	event.stopPropagation();
+
+});
+
+
+$("input[type='text']").on("keypress", function(event){
+	if(event.which === 13) {
+		//grabbing text for new todos
+		var todoText = $(this).val();
+		//puliamo il campo input 
+		$(this).val("");
+		//crea nuovo li e aggiungilo allo ul
+		$("ul").append("<li><span>X</span> " +todoText+" </li>");
+		
+	}
+});
+ 
+
+
+
 
